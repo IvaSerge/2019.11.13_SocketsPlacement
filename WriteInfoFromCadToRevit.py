02323bb02a25a777b85ae95ff8e0c73faad8439a
IvaSerge <ivaserge@ukr.net>
2019-11-13 23:17:27 +0100

    Create new Family Instance

--- WriteInfoFromCadToRevit.py
+++ WriteInfoFromCadToRevit.py
@@ -16,54 +16,92 @@ from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 doc = DocumentManager.Instance.CurrentDBDocument
 
+def mm_to_ft(mm):
+	return 3.2808*mm/1000
+
+def ft_to_mm(ft):
+	return ft*304.8
+
+def SetpParVal(elem, name, pValue):
+	global doc
+	try:
+		TransactionManager.Instance.EnsureInTransaction(doc)
+		elem.LookupParameter(name).Set(pValue)
+		TransactionManager.Instance.TransactionTaskDone()
+	except: pass
+
+
+
 class rvtElem:
 
 	def __init__(self, excelInfo):
-		global doc
-		self.doc = doc
+		#Excel elements
 		self.fName = i[8]
 		self.tName = i[9]
 		self.lvl = i[10]
 		self.h = i[11]
-		self.x = i[4]
-		self.y = i[5]
+		self.x = mm_to_ft(float(i[4]))
+		self.y = mm_to_ft(float(i[5]))
 		self.rotation = i[6]
+		
+		#Revit elements
+		global doc
+		self.doc = doc
+		self.rvtType = self._getType()
+		self.rvtLvl = self._getLevel()
+		self.rvtIns = self._newIns()
 
-	def getType(self): 
+	def _getType(self): 
+		"""Найти в проекте семейство и тип"""
 		testFam = BuiltInParameter.ALL_MODEL_FAMILY_NAME
 		pvpF = ParameterValueProvider(ElementId(int(testFam)))
 		fnrvStr = FilterStringEquals()
 		filterF = ElementParameterFilter(FilterStringRule(
 							pvpF, fnrvStr, self.fName, False))
-		
 		testType = BuiltInParameter.SYMBOL_NAME_PARAM
 		pvpT = ParameterValueProvider(ElementId(int(testType)))
 		filterT = ElementParameterFilter(FilterStringRule(
 							pvpT, fnrvStr, self.tName, False))
-		
 		filterAnd = LogicalAndFilter(filterF, filterT)
-		
 		rvtType = FilteredElementCollector(self.doc)\
 			.OfClass(FamilySymbol)\
 			.WherePasses(filterAnd)\
 			.FirstElement()
-		
 		return rvtType
+
+	def _getLevel(self): 
+		"""Найти в проекте уровень"""
+		testFam = BuiltInParameter.DATUM_TEXT
+		pvpF = ParameterValueProvider(ElementId(int(testFam)))
+		fnrvStr = FilterStringEquals()
+		filterC = ElementParameterFilter(FilterStringRule(
+							pvpF, fnrvStr, self.lvl, False))
+		rvtLvl = FilteredElementCollector(self.doc)\
+			.OfCategory(BuiltInCategory.OST_Levels)\
+			.WherePasses(filterC)\
+			.FirstElement()
+		return rvtLvl
+	
+	def _newIns(self):
+		pt = XYZ(self.x, self.y, 0)
+		tp = self.rvtType
+		if not (tp.IsActive):
+			tp.Activate()
+			doc.Regenerate()
+		lvl = self.rvtLvl
+		str = Structure.StructuralType.NonStructural
+		elem = self.doc.Create.NewFamilyInstance(pt, tp, lvl, str)
+		return elem
 
 info = IN[0]
 reload = IN[1]
-
 info.pop(0)
+
+TransactionManager.Instance.EnsureInTransaction(doc)
 
 rvtObj = [rvtElem(i) for i in info]
 
-#Найти в проекте семейство и тип
-typList = map(lambda x:x.getType(), rvtObj)
-
-#Преобразовать координаты и вставить по ним семейство
-
-#Развернуть семейство
-
 #Вписать информацию в семейство
+TransactionManager.Instance.EnsureInTransaction(doc)
 
-OUT = typList
+OUT = map(lambda x:x.rvtIns, rvtObj)
